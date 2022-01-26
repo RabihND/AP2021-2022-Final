be9f34ae427a2f43c148e8ce6fc8b2059ee554c8
RabihND <rabih.nd99@hotmail.com>
2022-01-26 08:59:50 +0330

    Add __main__

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ class GameOverWindow:
     """The resulting window, which shows the player's loss(+) or victory(-)"""
     pass
 
+if __name__ == "__main__":
+    import sys
+    mastermind = Mastermind()
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow(mastermind)
+    ui.setupUi(MainWindow, mastermind)
+    MainWindow.show()
+    sys.exit(app.exec_())
 
 
 
