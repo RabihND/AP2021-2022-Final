1e735f78ff485af8863833ccb507e31021dce0e4
RabihND <rabih.nd99@hotmail.com>
2022-01-26 22:36:18 +0330

    Update "Game Over Window" Class

--- gameoverwindow.py
+++ gameoverwindow.py
@@ -2,6 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class GameOverWindow(QtWidgets.QDialog):
+    """The resulting window, which shows the player's loss(+) or victory(-)"""
     def __init__(self, text):
         super(GameOverWindow, self).__init__()
         self.text = text
@@ -14,8 +15,7 @@ class GameOverWindow(QtWidgets.QDialog):
         self.buttonBox = QtWidgets.QDialogButtonBox(GameOverWindow)
         self.buttonBox.setGeometry(QtCore.QRect(-120, 110, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(
-            QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
         self.label = QtWidgets.QLabel(GameOverWindow)
         self.label.setGeometry(QtCore.QRect(70, -10, 171, 111))
@@ -28,10 +28,13 @@ class GameOverWindow(QtWidgets.QDialog):
 
         self.display_text()
 
-        def display_text(self):
+    def display_text(self):
             self.label.setText("You " + self.text + " the game. \nWant to try again?")
+            self.label.setStyleSheet("font-family: Kristen ITC;\n"
+                                                "font-size: 10pt;\n"
+                                                "font-weight: normal;")
 
-        def retranslateUi(self, gameoverDialog):
+    def retranslateUi(self, gameoverDialog):
             _translate = QtCore.QCoreApplication.translate
             gameoverDialog.setWindowTitle( _translate("GAME OVER", "Game over"))
             self.label.setText(_translate("GAMEOVER", "You Won !!. \nWant to try again?"))
--- mainwindow.py
+++ mainwindow.py
@@ -196,20 +196,4 @@ class MainWindow(object):
 
 
 
-class GameOverWindow:
-    """The resulting window, which shows the player's loss(+) or victory(-)"""
-    pass
-
-if __name__ == "__main__":
-    import sys
-    mastermind = Mastermind()
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow(mastermind)
-    ui.setupUi(MainWindow, mastermind)
-    MainWindow.show()
-    sys.exit(app.exec_())
-
-
-
 
